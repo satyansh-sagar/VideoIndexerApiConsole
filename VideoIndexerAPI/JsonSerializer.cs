2a5dff2f8a43786cabf1aeaf05ff70f37af185b9
Adwait Tulapurkar (Tech Mahindra Ltd) <v-adtula@microsoft.com>
2017-09-08 17:22:06 +0530

    Initial commit

---
+++ VideoIndexerAPI/JsonSerializer.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoIndexerAPI
+{
+
+    public class JsonSerializer
+    {
+        public string accountId { get; set; }
+        public string id { get; set; }
+        public string partition { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string userName { get; set; }
+        public DateTime createTime { get; set; }
+        public string organization { get; set; }
+        public string privacyMode { get; set; }
+        public string state { get; set; } 
+        public bool isOwned { get; set; }
+        public bool isEditable { get; set; }
+        public bool isBase { get; set; }
+        public int durationInSeconds { get; set; }
+        public Summarizedinsights summarizedInsights { get; set; }
+        public Breakdown[] breakdowns { get; set; }
+        public Social social { get; set; }
+    }
+
+    public class Summarizedinsights
+    {
+        public string name { get; set; }
+        public string shortId { get; set; }
+        public int privacyMode { get; set; }
+        public Duration duration { get; set; }
+        public string thumbnailUrl { get; set; }
+        public object[] faces { get; set; }
+        public Topic[] topics { get; set; }
+        public Sentiment[] sentiments { get; set; }
+        public Audioeffect[] audioEffects { get; set; }
+        public Annotation[] annotations { get; set; }
+    }
+
+    public class Duration
+    {
+        public string time { get; set; }
+        public float seconds { get; set; }
+    }
+
+    public class Topic
+    {
+        public string name { get; set; }
+        public Appearance[] appearances { get; set; }
+        public bool isTranscript { get; set; }
+        public int id { get; set; }
+    }
+
+    public class Appearance
+    {
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public float startSeconds { get; set; }
+        public float endSeconds { get; set; }
+    }
+
+    public class Sentiment
+    {
+        public string sentimentKey { get; set; }
+        public Appearance1[] appearances { get; set; }
+        public float seenDurationRatio { get; set; }
+    }
+
+    public class Appearance1
+    {
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public float startSeconds { get; set; }
+        public float endSeconds { get; set; }
+    }
+
+    public class Audioeffect
+    {
+        public string audioEffectKey { get; set; }
+        public Appearance2[] appearances { get; set; }
+        public float seenDurationRatio { get; set; }
+        public float seenDuration { get; set; }
+    }
+
+    public class Appearance2
+    {
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public float startSeconds { get; set; }
+        public float endSeconds { get; set; }
+    }
+
+    public class Annotation
+    {
+        public string name { get; set; }
+        public Appearance3[] appearances { get; set; }
+    }
+
+    public class Appearance3
+    {
+        public string startTime { get; set; }
+        public string endTime { get; set; }
+        public float startSeconds { get; set; }
+        public float endSeconds { get; set; }
+    }
+
+    public class Social
+    {
+        public bool likedByUser { get; set; }
+        public int likes { get; set; }
+        public int views { get; set; }
+    }
+
+    public class Breakdown
+    {
+        public string accountId { get; set; }
+        public string id { get; set; }
+        public string state { get; set; }
+        public string processingProgress { get; set; }
+        public string failureCode { get; set; }
+        public string failureMessage { get; set; }
+        public object externalId { get; set; }
+        public object externalUrl { get; set; }
+        public object metadata { get; set; }
+        public Insights insights { get; set; }
+        public string thumbnailUrl { get; set; }
+        public string publishedUrl { get; set; }
+        public string viewToken { get; set; }
+        public string sourceLanguage { get; set; }
+        public string language { get; set; }
+    }
+
+    public class Insights
+    {
+        public Transcriptblock[] transcriptBlocks { get; set; }
+        public Topic1[] topics { get; set; }
+        public object[] faces { get; set; }
+        public Participant[] participants { get; set; }
+        public Contentmoderation contentModeration { get; set; }
+        public Audioeffectscategory[] audioEffectsCategories { get; set; }
+    }
+
+    public class Contentmoderation
+    {
+        public float adultClassifierValue { get; set; }
+        public int bannedWordsCount { get; set; }
+        public float bannedWordsRatio { get; set; }
+        public bool isSuspectedAsAdult { get; set; }
+        public bool isAdult { get; set; }
+    }
+
+    public class Transcriptblock
+    {
+        public int id { get; set; }
+        public Line[] lines { get; set; }
+        public object[] sentimentIds { get; set; }
+        public object[] thumbnailsIds { get; set; }
+        public float sentiment { get; set; }
+        public object[] faces { get; set; }
+        public Ocr[] ocrs { get; set; }
+        public Audioeffectinstance[] audioEffectInstances { get; set; }
+        public Scene[] scenes { get; set; }
+        public Annotation1[] annotations { get; set; }
+    }
+
+    public class Line
+    {
+        public int id { get; set; }
+        public Timerange timeRange { get; set; }
+        public Adjustedtimerange adjustedTimeRange { get; set; }
+        public int participantId { get; set; }
+        public string text { get; set; }
+        public bool isIncluded { get; set; }
+        public float confidence { get; set; }
+    }
+
+    public class Timerange
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Adjustedtimerange
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Ocr
+    {
+        public Timerange1 timeRange { get; set; }
+        public Adjustedtimerange1 adjustedTimeRange { get; set; }
+        public Line1[] lines { get; set; }
+    }
+
+    public class Timerange1
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Adjustedtimerange1
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Line1
+    {
+        public int id { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public string language { get; set; }
+        public string textData { get; set; }
+        public float confidence { get; set; }
+    }
+
+    public class Audioeffectinstance
+    {
+        public int type { get; set; }
+        public Range[] ranges { get; set; }
+    }
+
+    public class Range
+    {
+        public int type { get; set; }
+        public Timerange2 timeRange { get; set; }
+    }
+
+    public class Timerange2
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Scene
+    {
+        public int id { get; set; }
+        public Timerange3 timeRange { get; set; }
+        public string keyFrame { get; set; }
+        public Shot[] shots { get; set; }
+    }
+
+    public class Timerange3
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Shot
+    {
+        public int id { get; set; }
+        public Timerange4 timeRange { get; set; }
+        public string keyFrame { get; set; }
+    }
+
+    public class Timerange4
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Annotation1
+    {
+        public string name { get; set; }
+        public Timerange5[] timeRanges { get; set; }
+        public Adjustedtimerange2[] adjustedTimeRanges { get; set; }
+    }
+
+    public class Timerange5
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Adjustedtimerange2
+    {
+        public string start { get; set; }
+        public string end { get; set; }
+    }
+
+    public class Topic1
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string stem { get; set; }
+        public string[] words { get; set; }
+        public float rank { get; set; }
+    }
+
+    public class Participant
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public object pictureUrl { get; set; }
+    }
+
+    public class Audioeffectscategory
+    {
+        public int type { get; set; }
+        public string key { get; set; }
+    }
+
+}
---
+++ VideoIndexerAPI/Program.cs
@@ -0,0 +1,118 @@
+﻿
+
+///Author : Satyansh Sagar(satyansh.sagar@outlook.com)
+///Date : 06-02-2017
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VideoIndexerAPI
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            indexer();
+            Console.WriteLine("press 'Enter' to Exit");
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        private static void indexer()
+        {
+            var key = "6a4ae6e259eb401492514d38cd78dcfc";
+            var apiUrl = "https://videobreakdown.azure-api.net/Breakdowns/Api/Partner/Breakdowns";
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", key);
+
+            var videoPath = @"D:\test1.mp4";
+            
+            var content = new MultipartFormDataContent();
+            //content.Add(new StreamContent(File.Open(videoPath, FileMode.Open)), "Video", "Video");
+
+            content.Add(new StreamContent(File.Open(videoPath, FileMode.Open)));
+
+            Console.WriteLine("Uploading...");
+            var result = client.PostAsync(apiUrl + "?name=some_name&description=some_description&privacy=private&partition=some_partition", content).Result;
+            var json = result.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine();
+            Console.WriteLine("Uploaded:");
+            Console.WriteLine(json);
+
+            var id = JsonConvert.DeserializeObject<string>(json);
+            
+            while (true)
+            {
+                Thread.Sleep(10000);
+
+                result = client.GetAsync(string.Format(apiUrl + "/{0}/State", id)).Result;
+                json = result.Content.ReadAsStringAsync().Result;
+
+                Console.WriteLine();
+                Console.WriteLine("State:");
+                Console.WriteLine(json);
+
+                dynamic state = JsonConvert.DeserializeObject(json);
+                if (state.state != "Uploaded" && state.state != "Processing")
+                {
+                    break;
+                }
+            }
+
+            //result = client.GetAsync(string.Format(apiUrl + "/{0}", id)).Result;
+            //json = result.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine();
+            //Console.WriteLine("Full JSON:");
+            //Console.WriteLine(json);
+
+            //result = client.GetAsync(string.Format(apiUrl + "/Search?id={0}", id)).Result;
+            //json = result.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine();
+            //Console.WriteLine("Search:");
+            //Console.WriteLine(json);
+
+            //result = client.GetAsync(string.Format(apiUrl + "/{0}/InsightsWidgetUrl", id)).Result;
+            //json = result.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine();
+            //Console.WriteLine("Insights Widget url:");
+            //Console.WriteLine(json);
+
+            //result = client.GetAsync(string.Format(apiUrl + "/{0}/PlayerWidgetUrl", id)).Result;
+            //json = result.Content.ReadAsStringAsync().Result;
+            //Console.WriteLine();
+            //Console.WriteLine("Player token:");
+            //Console.WriteLine(json);
+
+
+            
+
+            result = client.GetAsync(string.Format(apiUrl + "/{0}", id)).Result;
+            var jsonRes = result.Content.ReadAsStringAsync().Result;
+            var replyMsg = JsonConvert.DeserializeObject<JsonSerializer>(jsonRes);
+
+
+            foreach (var i in replyMsg.breakdowns) 
+            {
+                foreach(var j in i.insights.transcriptBlocks)
+                {
+                    Console.WriteLine("Sentiment : "+j.sentiment);
+                    foreach(var k in j.lines)
+                    {
+                        Console.WriteLine(k.text);
+                    }
+                }
+            }
+        }
+    }
+}
